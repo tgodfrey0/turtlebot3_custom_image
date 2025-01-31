74c4fe090a1f6e0901b72f05a4e3a2e524f45474
Toby Godfrey <tmag1g21@soton.ac.uk>
2025-01-31 15:54:08 +0000

    correct hostname

--- files/setup_hostname.sh
+++ files/setup_hostname.sh
@@ -2,7 +2,8 @@
 set -eux -o pipefail
 
 if [[ -f /root/.setup_hostname ]]; then
-  NEW_HOSTNAME="turtlebot_$(ip -brief link show | grep -v LOOPBACK | grep -v "DOWN" | head -n 1 | sed 's/ /\n/g' | grep -v "^$" | head -n 3 | tail -n 1 | cut -d ':' -f4- | tr ":" "_")"
+  pip install uuid
+  NEW_HOSTNAME=$(python /root/get_hostname.py)
   echo -e "\e[1;32mSetting hostname to $NEW_HOSTNAME\e[0m"
   cat /etc/cloud/cloud.cfg | sed 's/preserve_hostname: false/preserve_hostname: true' > /etc/cloud/cloud.cfg
   hostnamectl set-hostname $NEW_HOSTNAME
