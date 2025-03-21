3d07fc2fd7d9e42e1b39dec4ed81d189b353d8a4
Toby Godfrey <tmag1g21@soton.ac.uk>
2025-03-21 14:24:47 +0000

    Fix SSH service name and include OpenCR model in image name

--- scripts/30_general_system_setup.sh
+++ scripts/30_general_system_setup.sh
@@ -5,7 +5,7 @@ echo -e "\e[1;32mConfiguring system\e[0m"
 echo -e 'APT::Periodic::Update-Package-Lists "0";\nAPT::Periodic::Unattended-Upgrade "0";' > /etc/apt/apt.conf.d/20auto-upgrades
 systemctl mask systemd-networkd-wait-online.service
 systemctl mask sleep.target suspend.target hibernate.target hybrid-sleep.target
-systemctl enable sshd
+systemctl enable ssh
 
 sed -i 's/^#\?PasswordAuthentication.*/PasswordAuthentication yes/' /etc/ssh/sshd_config
 sed -i 's/^#\?PubkeyAuthentication.*/PubkeyAuthentication no/' /etc/ssh/sshd_config
